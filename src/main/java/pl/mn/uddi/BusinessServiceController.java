55928f132c51a21ba17f8d7b312182cf0706589e
Lidjan <minidmnv@gmail.com>
2017-04-25 20:44:13 +0200

    Commit inicjalizujący - opisujący podstawowe struktury

---
+++ src/main/java/pl/mn/uddi/BusinessServiceController.java
@@ -0,0 +1,13 @@
+package pl.mn.uddi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author minidmnv
+ */
+@RestController
+@RequestMapping
+public class BusinessServiceController {
+
+}
